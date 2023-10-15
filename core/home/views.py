941a8c289b5f77c01d371102fbc01016dbeede4e
Aneesh02 <aneeshtickoo2002@gmail.com>
2023-10-15 16:06:08 +0530

    Intialised the basic web app in Django

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout, login
+from django.http import HttpResponse
+from home.models import Photo, Person, PersonGallery
+import re
+
+
+# ReGEx required for getting photo name
+post_type = re.compile(r"static/images/(.*)")
+
 
 # Create your views here.
+def landing(request):
+    return render(request, "landing.html")
+
+def loginUser(request):
+    pass
+
+
