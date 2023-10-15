5a6c5f6f82c39fbeb4f6e9da8022d7b9ee7912a0
Vishesh Thakur <thakurvishesh1@gmail.com>
2023-10-15 17:08:30 +0530

    Update views.py

--- core/home/views.py
+++ core/home/views.py
@@ -106,6 +106,71 @@ def process(request):
     labelIDs = np.unique(clt.labels_)
     numUniqueFaces = len(np.where(labelIDs > -1)[0])
 
+    for labelID in labelIDs:
+        idxs = np.where(clt.labels_ == labelID)[0]
+        owner_pic = data[idxs[0]]["imagePath"]
+        image = cv2.imread(owner_pic)
+        (top, right, bottom, left) = data[idxs[0]]["loc"]
+        face = image[top:bottom, left:right]
+        face = cv2.resize(face, (96, 96))
+        face_img_path = f"{user.get_username()}_owner{labelID}.jpg"
+        cv2.imwrite(f"static/images/{face_img_path}", face)
+        person = Person.objects.create(user=user, thumbnail=face_img_path)
+        person.save()
 
+        for i in idxs:
+            src_direc = data[i]["imagePath"]
+            link = post_type.search(src_direc)
+            personGallery = PersonGallery.objects.create(
+                person=person, image=str(link.group(1))
+            )
+            personGallery.save()
+    user = request.user
+    persons = Person.objects.filter(user=user)
+
+    score_list = []
+    numUniqueFaces = persons.count()
+    path = os.getcwd() + str("/static/images/")
+
+    for i in range(numUniqueFaces):
+        intmd_lst = []
+        for j in range(i + 1, numUniqueFaces):
+            image1 = cv2.imread(path + str(persons[i].thumbnail))
+            image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+            image2 = cv2.imread(path + str(persons[j].thumbnail))
+            image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+            score = dist.cosine(image1.reshape(-1), image2.reshape(-1))
+            intmd_lst.append([i, j, score])
+            print(i, j, score)
+        score_list.append(intmd_lst)
+
+    result = []
+    unsorted_person = [i for i in range(1, numUniqueFaces)]
+    sorted_person = []
+    start = 0
+    next = score_list[0][0][1]
+    sorted_person.append(start)
+    i = 0
+    for _ in range(numUniqueFaces - 1):
+        print(score_list[start])
+        min = 1
+        for ele in score_list[start]:
+            if ele[2] < min:
+                min = ele[2]
+                next = ele[1]
+        print(min, next)
+
+        if start == next:
+            break
+        else:
+            unsorted_person.remove(next)
+            sorted_person.append(next)
+            start = next
+    final = sorted_person + unsorted_person
+    for ele in final:
+        result.append(persons[ele])
+
+    context = {"persons": result, "faces": numUniqueFaces}
+    return render(request, "process.html", context)
 
 
